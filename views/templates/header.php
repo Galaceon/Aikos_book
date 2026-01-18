41b272cc88bc655a74e15fe24217f526de45bc80
Galaceon <antoniogarcialtr@gmail.com>
2026-01-18 01:43:25 +0100

    desktop header done

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,1 +1,51 @@
-hola
+ <div class="barra">
+    <div class="barra__contenido">
+
+            <!-- LOGOTIPO -->
+            <a href="/" class="barra__enlace" data-value="AIKO'S BOOK">
+                <div class="barra__logo">
+                    <span class="barra__logotipo material-symbols-outlined">auto_stories</span>
+                    <h2 class="barra__titulo">AIKO'S BOOK</h2>
+                </div>
+            </a>
+
+            <!-- BUSCADORES -->
+            <div class="barra__buscadores">
+                <!-- BUSCADOR -->
+                <div class="barra__buscador">
+                    <span class="barra__search-img material-symbols-outlined">search</span>
+                    <input class="barra__input" id="buscador" type="text" placeholder="Buscar reseña...">
+                </div> 
+
+                <!-- FILTROS -->
+                <div class="barra__filtros">
+                    Tags
+                    <span class="barra__symbol material-symbols-outlined">keyboard_arrow_down</span>
+                </div>
+        </div>
+        <nav class="barra__nav">
+            <a href="/">
+                <div class="barra__nav-enlaces">
+                    <span class="barra__search-img material-symbols-outlined">frame_person</span>
+                    <p class="barra__nav-text">PERFIL</p>
+                </div>
+            </a>
+            <a href="/">
+                <div class="barra__nav-enlaces">
+                    <span class="barra__search-img material-symbols-outlined">bookmarks</span>
+                    <p class="barra__nav-text">GUARDADOS</p>
+                </div>
+            </a>
+            <a href="/">
+                <div class="barra__nav-enlaces">
+                    <span class="barra__search-img material-symbols-outlined">login</span>
+                    <p class="barra__nav-text">LOGIN</p>
+                </div>
+            </a>
+        </nav>
+        <div class="barra__darkmode">
+            <div class="barra__darkmode-contenedor">
+                <span class="material-symbols-outlined">brightness_4</span>
+            </div>
+    </div>
+ </div>
