0d05eb062407d2a84374e759fe54ca7f51cb3a03
Galaceon <antoniogarcialtr@gmail.com>
2026-01-19 23:44:19 +0100

    mobile sidebar no-scroll fix

--- src/js/header.js
+++ src/js/header.js
@@ -2,42 +2,28 @@
     const abrirMenu = document.querySelector('.barra__mobile-button');
     const menu = document.querySelector('.barra__mobile-menu');
 
-    // Responsive Sidebar for normal users
     if (abrirMenu) {
         abrirMenu.addEventListener('click', () => {
-            // Estado del sidebar(oculto o mostrandose)
-            const menuState = menu.classList.value.includes('mostrar');
+            const menuState = menu.classList.contains('mostrar');
 
-            if(menuState) {
+            if (menuState) {
                 menu.classList.remove('mostrar');
-                document.body.classList.add('menu-abierto');
+                document.body.classList.remove('menu-abierto');
             } else {
                 menu.classList.add('mostrar');
                 document.body.classList.add('menu-abierto');
             }
+
+            abrirMenu.classList.toggle('active');
         });
     }
 
     window.addEventListener('resize', () => {
-        // Estado del boton hamburguesa
-        const abrirMenuState = abrirMenu.classList.value.includes('active');
-
-        // Si la pantalla es mayot o igual a tablet el menu mobile se oculta y el boton hamburguesa se queda en X
         if (window.innerWidth >= 768) {
             menu.classList.remove('mostrar');
             document.body.classList.remove('menu-abierto');
-
-            if(abrirMenuState) {
-                abrirMenu.classList.remove('active');
-            }
+            abrirMenu.classList.remove('active');
         }
     });
-
-
-    // Hamburger button animation
-    document.querySelectorAll('.barra__mobile-button').forEach(btn => {
-        btn.addEventListener('click', e => {
-            btn.classList.toggle('active');
-        });
-    });
 })();
+
