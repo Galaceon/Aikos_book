644aa600c679fb163ab74ddd62b9aaea227cddea
Galaceon <antoniogarcialtr@gmail.com>
2026-01-21 19:19:01 +0100

    logout feature added

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -9,7 +9,6 @@ class PagesController {
 
     public static function index(Router $router) {
 
-        debuguear($_SESSION);
 
         $router->render('pages/index', [
             'titulo' => 'Aikos Book'
