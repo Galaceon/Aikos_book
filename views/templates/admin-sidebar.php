fa38cf0232e98a88d1932bf09ba160df6c22e853
Galaceon <antoniogarcialtr@gmail.com>
2026-01-31 20:16:45 +0100

    admin sidebar fixed

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -50,4 +50,15 @@
         <span class="dashboard__sidebar-icon material-symbols-outlined">groups</span>
         <p class="dashboard__sidebar-text">REGISTRADOS</p>
     </a>
+
+    <div class="barra__darkmode--mobile">
+        <div class="barra__darkmode-contenedor--mobile">
+            <span class="material-symbols-outlined">brightness_4</span>
+        </div>
+    </div>
+
+    <form method="POST" action="/logout" class="dashboard__enlace">
+        <label for="cerrar_sesion" class="dashboard__sidebar--mobile__label"><span class="barra__search-img material-symbols-outlined">login</span></label>
+        <input id="cerrar_sesion" type="submit" value="LOGOUT" class="dashboard__sidebar--mobile__submit">
+    </form>
 </aside>
